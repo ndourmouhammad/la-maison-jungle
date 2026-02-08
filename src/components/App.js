d9af94ddb0b818d6700d1266035cf0912ad192ff
ndourmouhammad <ndourmouhammad15@gmail.com>
2026-02-08 11:58:12 +0000

    feat: Implement plant listing feature including React components, styles, assets, and plant data.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import Banner from './Banner'
-import Cart from './Cart'
+// import Cart from './Cart'
+import ShoppingList from './ShoppingList'
+
 
 function App() {
-    return (
-        <div>
-            <Banner />
-            <Cart />
-        </div>
-    )
+	return (
+		<div>
+			<Banner />
+			{/* <Cart /> */}
+			<ShoppingList />
+		</div>
+	)
 }
 
 export default App
